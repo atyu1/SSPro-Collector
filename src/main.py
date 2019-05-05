8821c5c7e701b5cf6add9fd7faecf6481e5127ce
atyu1 <attila.sovak@gmail.com>
2019-05-05 13:22:19 +0200

    Add first humidity temperature readers

--- src/main.py
+++ src/main.py
@@ -1,3 +1,10 @@
 # TODO
 # main file
+from sensors.humidity_temperature import humidity_sensor as HUMIDITY
+from sensors.humidity_temperature import temperature_sensor as TEMPERATURE
 
+HUM_SEN = HUMIDITY()
+TEMP_SEN = TEMPERATURE()
+
+print (HUM_SEN.run())
+print (TEMP_SEN.run())
