2e5b84513732070a48cf5982e4a24dd2cc77f9b8
root <root@localhost.localdomain>
2019-06-24 23:33:25 +0200

    Add basic sensor selector based on config file content

--- src/main.py
+++ src/main.py
@@ -3,15 +3,28 @@
 from sensors.humidity_temperature import humidity_sensor as HUMIDITY
 from sensors.humidity_temperature import temperature_sensor as TEMPERATURE
 from sender import JsonSender
+from config import ConfigReader
+
+config_content = ConfigReader()
 
 data = []
-
-sensors = [ HUMIDITY(), 
-            TEMPERATURE(),
-          ]
+sensors = sensor_modularize(config_content)
 
 for sensor in sensors:
     data.append(sensor.run())
-
+print (data)
 sender = JsonSender("https://localhost/test")
 sender.push()
+
+def sensor_modularize(config):
+    """ Create a list of modules based on config file enabled sensors """
+   
+    tmp_sensors = []
+
+    if "humidity" in config.sensors:
+        tmp_sensors.append(HUMIDITY())
+
+    if "temperature" in config.sensors:
+        tmp_sensors.append(TEMPERATURE())
+
+    return tmp_sensors
