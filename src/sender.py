81d3d69aa6d1b2d445c759649b6c369a77916323
root <root@localhost.localdomain>
2019-06-24 23:04:41 +0200

    Fix sender tabs

--- src/sender.py
+++ src/sender.py
@@ -9,17 +9,17 @@ class JsonSender:
     def __init__(self, url, data, user="", password="", secure=False):
         """ Basic initiator """
         self.ipaddr = ipaddr
-	self.port = port
+        self.port = port
         self.user = user
         self.password = password
 
         self.data = self._fix_data(data)
 
     def _fix_data(self, data):
-	""" Review the collected data and check if all fields are present"""
-	# Todo: Add data pre-checks
+        """ Review the collected data and check if all fields are present"""
+        # Todo: Add data pre-checks
 
-	return data
+        return data
 
     def push(self):
         """ Push data to server """
