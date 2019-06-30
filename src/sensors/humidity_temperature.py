b19a8c70d9f0a1d5442c329e8be282f55a16a26f
atyu1 <attila.sovak@gmail.com>
2019-06-30 14:39:14 +0200

    Fix to use base DHT11 sensor class

--- src/sensors/humidity_temperature.py
+++ src/sensors/humidity_temperature.py
@@ -1,75 +1,19 @@
-import RPi.GPIO as GPIO
-from DHT11_Python import dht11
-import json
+from sensor import DHT11_Sensor
 
-class dht11_sensor:
-	"""TODO: """
-	pass 
-
-class humidity_sensor(dht11_sensor):
+class humidity_sensor(DHT11_Sensor):
     """
-    Class for direct work with sensor and get raw data from DHT11 sensor
+    Class for direct work with sensor and get raw data from DHT11 sensor,
+    we target in this class for humidity
     """
 
-    def __init__(self, pin=4):
-        # initialize GPIO
-        GPIO.setwarnings(False)
-        GPIO.setmode(GPIO.BCM)
-        GPIO.cleanup()
-
-        self._reader = dht11.DHT11(pin=pin)
-        self._raw_data = self._read_raw()
-
-    def _read_raw(self):
-        "Read the digits from sensor directly"""
-        result = self._reader.read()
-        return result
-
-    def get_data(self):
-        """ Get results in dictionary """
-        data = {"humidity":self._raw_data.humidity}
-        return data
-
-    def print_text(self):
-        """ Print the results for local testing """
-        data = "Temperature: %d\nHumidity: %d\n" % (self._raw_data.temperature,
-                self._raw_data.humidity)
-
-        print (data) 
-
-    def run(self):
-        """ Main run file to run this module """
-        return self.get_data()
-
-class temperature_sensor(dht11_sensor):
+    def __init__(self, pin=4, key_name="humidity"):
+        super().__init__(pin=pin, key_name=key_name)
+       
+class temperature_sensor(DHT11_Sensor):
     """
-    Class for direct work with sensor and get raw data from DHT11 sensor
+    Class for direct work with sensor and get raw data from DHT11 sensor,
+    we target in this class for temperature
     """
 
-    def __init__(self, pin=4):
-        # initialize GPIO
-        GPIO.setwarnings(False)
-        GPIO.setmode(GPIO.BCM)
-        GPIO.cleanup()
-
-        self._reader = dht11.DHT11(pin=pin)
-        self._raw_data = self._read_raw()
-
-    def _read_raw(self):
-        "Read the digits from sensor directly"""
-        result = self._reader.read()
-        return result
-
-    def get_data(self):
-        """ Get results in dictionary """
-        data = {"temperature":self._raw_data.temperature} 
-        return data
-
-    def print_text(self):
-        """ Print the results for local testing """
-        data = "Temperature: %d\n" % (self._raw_data.temperature)
-        print (data) 
-
-    def run(self):
-        """ Main run file to run this module """
-        return self.get_data()
+    def __init__(self, pin=4, key_name="temperature"):
+        super().__init__(pin=pin, key_name=key_name)
