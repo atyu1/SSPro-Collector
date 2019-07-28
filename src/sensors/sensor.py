181e5422a8bc3f59e63029b5b630c793872a1c13
atyu1 <attila.sovak@gmail.com>
2019-07-28 13:57:23 +0200

    Split sensors and dht11 sensors file

--- src/sensors/sensor.py
+++ src/sensors/sensor.py
@@ -1,54 +1,12 @@
-import RPi.GPIO as GPIO
-from DHT11_Python import dht11
+#import RPi.GPIO as GPIO
+#from DHT11_Python import dht11
 import json
 
-class SensorError(Excpetion):
+class SensorError(Exception):
     """ Sensor Issues are reported through custom error """
     pass
-
 
 class Sensor(SensorError):
     """ General class for common sensor attributes """
     pass
 
-
-class DHT11_Sensor(Sensor):
-    """
-    Class for direct work with sensor and get raw data from DHT11 sensor
-    """
-
-    def __init__(self, pin=4, key_name="missing"):
-        # initialize GPIO
-        GPIO.setwarnings(False)
-        GPIO.setmode(GPIO.BCM)
-        GPIO.cleanup()
-
-        self.key_name = key_name
-
-    def _dht11_reader(self):
-        self.reader = dht11.DHT11(pin=pin)
-        self.raw_data = self.reader.read()
- 
-    def __str__(self):
-        """ Print the results for local testing """
-        if not self.sensor_data: # Data collected only after run() fucntion called
-            return ""
-
-        return str(self.sensor_data)
-
-    def get_data(self):
-        """ Get results in dictionary """
-        if not self.raw_data.is_valid():
-            raise SensorError("Data from DHT11 is not valid")
-
-        if self.key_name == "humidity":
-            self.sensor_data = {keyname:self.raw_data.humidity}
-        elif self.key_name == "temperature":
-            self.sensor_data = {keyname:self.raw_data.temperature}
-
-        return self.sensor_data
-
-    def run(self):
-        """ Main run file to run this module """
-        self._dth11_reader()
-        return self.get_data()
