6defe6fb547967e5fe9303fd1ea6a76bbbece0ff
atyu1 <attila.sovak@gmail.com>
2019-07-28 15:47:59 +0200

    Fix dht11 sensor tests, WIP

--- src/test/test_sensors/test_dht11_sensor.py
+++ src/test/test_sensors/test_dht11_sensor.py
@@ -3,23 +3,50 @@ import pytest
 from src.test.test_sensors.test_sensor import TestSensor as Sensor
 from src.sensors.dht11_sensor import DHT11_Sensor
 
-class DHT11_Raw_Data(DHT11_Sensor):
+class DHT11_Fake_Sensor(DHT11_Sensor):
      """ This is class to use as data class for DHT11 tests """
-     def __init__(self, humidity=30, temperature=20):
-         self.humidity = humidity
-         self.temperature = temperature
+     def __init__(self, pin=1, humidity=30, temperature=20, key_name="test", sensor_data=10):
+         self.pin = pin
+         self.raw_data = self.RawData()
+         self.raw_data.humidity = humidity
+         self.raw_data.temperature = temperature
+         self.raw_data.valid = True
+         self.key_name = key_name
+         self.sensor_data = sensor_data 
+
+     class RawData:
+         def __init__(self):
+             pass
+
+         def is_valid(self):
+             return self.valid
 
 class TestDHT11_Sensor:
     """ General tests for DHT11_sensor functions """
-    def init_data(self, key_name):
-        self.raw_data = DHT11_Raw_Data(humidity=40,temperature=25)
+    def __init__(self):
+        self._dht11_reader()
+
+    def _dht11_reader(self):
+        self.reader = DHT11_Fake_Sensor()  #Leave default values
 
     def test_init(self):
-        pass
+        assert self.reader.pin == 1
+        assert self.reader.raw_data.humidity == 30
+        assert self.reader.raw_data.temperature == 20
+        assert self.reader.key_name == "test"
+        assert self.reader.sensor_data == 10
 
-    def test_str(self):
+    def test_get_data(self):
         """ Function to test __str__() """
         key1 = "humidity"
         key2 = "temperature"
+   
+        expected_data1 = {key1:self.reader.sensor_data}
+        expected_data2 = {key2:self.reader.sensor_data}
 
-        # Test before and after run() method
+        self.reader.key_name = key1
+        data1 = self.get_data()
+
+        assert data1 == expected_data1
+ 
+        
